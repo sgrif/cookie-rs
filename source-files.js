bc8a0c511c8ed6ec36fde20eaf0f4baa1b1c2aef
Deploy from Travis CI <>
2019-01-23 22:19:45 +0000

    Deploy c253a23703907302306a7f72fabdf0bd7aec95b0 to gh-pages

---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["cookie"] = {"name":"","dirs":[],"files":["builder.rs","delta.rs","draft.rs","jar.rs","lib.rs","parse.rs"]};
+createSourceSidebar();
